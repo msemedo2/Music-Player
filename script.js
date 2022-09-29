02e5fc8b198c2728213a148e297ba7ab798b09c9
unknown <miguel.semedo.2@outlook.com>
2022-09-29 18:31:56 +0100

    Implement Duration Functionality

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const progressContainer = document.getElementById('progress-container');
 const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -93,6 +95,24 @@ function updateProgressBar(e) {
 		// Update Progress Bar Width
 		const progressPercent = (currentTime / duration) * 100;
 		progress.style.width = `${progressPercent}%`;
+
+		// Calculate Display for Duration
+		const durationMinutes = Math.floor(duration / 60);
+		let durationSeconds = Math.floor(duration % 60);
+		if (durationSeconds < 10) {
+			durationSeconds = `${durationSeconds}`;
+		}
+		// Delay Switching Duration Element to avoid NaN
+		if (durationSeconds) {
+			durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+		}
+		// Calculate Display for Current Time
+		const currentMinutes = Math.floor(currentTime / 60);
+		let currentSeconds = Math.floor(currentTime % 60);
+		if (currentSeconds < 10) {
+			currentSeconds = `0${currentSeconds}`;
+		}
+		currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
 	}
 }
 

38b86d7168bbb011ef0b7200ef89459a1020ad49
unknown <miguel.semedo.2@outlook.com>
2022-09-30 09:33:19 +0100

    Implement Dynamic Progress Bar and Time on Click

--- script.js
+++ script.js
@@ -116,7 +116,17 @@ function updateProgressBar(e) {
 	}
 }
 
+// Set Progress Bar
+function setProgressBar(e) {
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const { duration } = music;
+	music.currentTime = (clickX / width) * duration;
+}
+
 // Event Listener
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
+music.addEventListener('ended', nextSong);
